eb51282ffbb20f568fde10a5a372313ab8dce3d2
Sid Nair <sidnair09@gmail.com>
2019-05-02 13:36:19 -0400

    permission fix

--- src/handlePermissions.js
+++ src/handlePermissions.js
@@ -15,14 +15,9 @@ export const requestPermissions = async (hasVideoAndAudio, CameraManager, permis
             title: permissionDialogTitle,
             message: permissionDialogMessage,
           });
-    
-          // On devices before SDK version 23, the permissions are automatically granted if they appear in the manifest,
-          // so check and request should always be true.
-          // https://github.com/facebook/react-native-website/blob/master/docs/permissionsandroid.md
-          const isAuthorized =
-            Platform.Version >= 23 ? granted === PermissionsAndroid.RESULTS.GRANTED : granted === true;
-    
-          return isAuthorized;
+
+      // See https://github.com/react-native-community/react-native-camera/pull/2099
+      return granted === PermissionsAndroid.RESULTS.GRANTED || granted === true;
     }
     return true;
 }
